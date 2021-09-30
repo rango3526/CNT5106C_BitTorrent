6cb7c00559ca599e237bcd11fc9b413e0643949b
Ranger Chenore <r2chenore@gmail.com>
2021-09-30 18:25:11 -0400

    Add new Client.java with un-implemented functions for structure

--- P2P File Share/src/Client.java
+++ P2P File Share/src/Client.java
@@ -5,20 +5,48 @@ import java.nio.channels.*;
 import java.util.*;
 
 public class Client {
-	Socket requestSocket;           //socket connect to the server
-	ObjectOutputStream out;         //stream write to the socket
-	ObjectInputStream in;          //stream read from the socket
-	String message;                //message send to the server
-	String MESSAGE;                //capitalized message read from the server
+	// Socket requestSocket;           //socket connect to the server
+	// ObjectOutputStream out;         //stream write to the socket
+	// ObjectInputStream in;          //stream read from the socket
+	// String message;                //message send to the server
+	// String MESSAGE;                //capitalized message read from the server
 
-	public void Client() {}
+    List<ObjectOutputStream> peerOuts;
+    List<ObjectInputStream> peerIns;
 
-	void run()
-	{
-		try{
-			//create a socket to connect to the server
-			requestSocket = new Socket("localhost", 8000);
-			System.out.println("Connected to localhost in port 8000");
+	public void SampleClient() {}
+
+    /*
+        PeerID is are the ones listed in the config file (1001, 1002, 1003, etc)
+    */
+
+    int getPortFromPeerID(int id) { // use the config file
+        throw new UnsupportedOperationException();
+    }
+
+    String getIPFromPeerID(int id) { // use the config file
+        throw new UnsupportedOperationException();
+    }
+
+    int peerIDToIndex(int id) { // Index in the peerOuts and peerIns
+        throw new UnsupportedOperationException();
+    }
+
+    List<Integer> getAllPeerIDs() {
+        throw new UnsupportedOperationException();
+    }
+
+    void connectToPeer(int id) {
+        String ip = getIPFromPeerID(id);
+        int port = getPortFromPeerID(id);
+
+        Socket requestSocket = null;           //socket connect to the server
+        ObjectOutputStream out = null;         //stream write to the socket
+        ObjectInputStream in = null;          //stream read from the socket
+
+        try {
+            requestSocket = new Socket(ip, port);
+			System.out.println("Connected to " + ip + " in port " + port);
 			//initialize inputStream and outputStream
 			out = new ObjectOutputStream(requestSocket.getOutputStream());
 			out.flush();
@@ -28,23 +56,25 @@ public class Client {
 			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 			while(true)
 			{
-				System.out.print("Hello, please input a sentence: ");
-				//read a sentence from the standard input
-				message = bufferedReader.readLine();
-				//Send the sentence to the server
-				sendMessage(message);
-				//Receive the upperCase sentence from the server
-				MESSAGE = (String)in.readObject();
-				//show the message to the user
-				System.out.println("Receive message: " + MESSAGE);
+				// Send messages to this peer here
+
+                // Send a handshake
+
+                // Look at the peer's bitfield
+
+                // Decide if we want their pieces
+
+                // And so on
+
+                throw new UnsupportedOperationException();
 			}
-		}
-		catch (ConnectException e) {
+        }
+        catch (ConnectException e) {
 			System.err.println("Connection refused. You need to initiate a server first.");
 		} 
-		catch ( ClassNotFoundException e ) {
-			System.err.println("Class not found");
-		} 
+		// catch (ClassNotFoundException e ) {
+		// 	System.err.println("Class not found");
+		// } 
 		catch(UnknownHostException unknownHost){
 			System.err.println("You are trying to connect to an unknown host!");
 		}
@@ -62,24 +92,30 @@ public class Client {
 				ioException.printStackTrace();
 			}
 		}
-	}
-	//send a message to the output stream
-	void sendMessage(String msg)
+    }
+
+    void connectToAllPeers() {
+        for (int id : getAllPeerIDs()) {
+            connectToPeer(id);
+        }
+    }
+
+	// //send a message to the output stream
+	// void sendMessage(int peerID)
+	// {
+	// 	try{
+	// 		//stream write the message
+	// 		out.writeObject(msg);
+	// 		out.flush();
+	// 	}
+	// 	catch(IOException ioException){
+	// 		ioException.printStackTrace();
+	// 	}
+	// }
+
+    public static void main(String args[])
 	{
-		try{
-			//stream write the message
-			out.writeObject(msg);
-			out.flush();
-		}
-		catch(IOException ioException){
-			ioException.printStackTrace();
-		}
-	}
-	//main method
-	public static void main(String args[])
-	{
-		Client client = new Client();
+		SampleClient client = new SampleClient();
 		client.run();
 	}
-
 }
