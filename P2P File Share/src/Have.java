d43c4100b89b9d67fbb4d66811baee1072da8aab
dbuf21 <davidbaker1@ufl.edu>
2021-10-20 18:59:52 -0400

    have/interested method
    added send methods for have and interested

--- P2P File Share/src/Have.java
+++ P2P File Share/src/Have.java
@@ -1,3 +1,13 @@
+
+
 public class Have {
-    
+    public static final int TYPE = 4;
+
+    public Have() {
+
+    }
+
+    public byte[] sendHaveMessage(int pieceIndex) {
+        // construct byte array with correct format
+    }
 }
--- P2P File Share/src/InterestHandler.java
+++ P2P File Share/src/InterestHandler.java
@@ -2,5 +2,13 @@
 
 public class InterestHandler {
     // TODO: David
-    
+    public static final int TYPE = 2;
+
+    public InterestHandler() {
+
+    }
+
+    public byte [] sendInterestMessage() {
+        // return byte array of correct format
+    }
 }
