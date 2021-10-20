0c08a455de34fe3babe0c4313253c31b50495604
dbuf21 <davidbaker1@ufl.edu>
2021-10-20 19:50:45 -0400

    handshake
    added sendhandshake method

--- P2P File Share/src/Handshake.java
+++ P2P File Share/src/Handshake.java
@@ -1,6 +1,24 @@
 public class Handshake {
 
 	// TODO: David
+
+	private final String header = "P2PFILESHARINGPROJ";
+    private final String bits = "0000000000";
+    private int peerId;
+	
+	Handshake() {
+
+	}
+
+	public Handshake(int peerId) {
+		this.peerId = peerId;
+	}
+
+	public byte[] sendHandshake() {
+		return (header + bits + peerId).getBytes();
+
+	}
+
     public static byte[] getHandshakeMessage(int fromClientID, int toPeerID) {
 		throw new UnsupportedOperationException();
 	}
