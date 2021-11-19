38d45592f6a489cb66a054ca96aab853c78a52ed
Tre' Jeter <33637310+tjeter@users.noreply.github.com>
2021-11-18 21:32:11 -0500

    Update Bitfield.java

--- P2P File Share/src/Bitfield.java
+++ P2P File Share/src/Bitfield.java
@@ -1,78 +1,127 @@
-
 import java.util.HashMap;
-import java.io.IOException;
 import java.util.*;
 
-public class Bitfield {
+public class Bitfield 
+{
     private static int pieceAmt = -1;
     private static int selfClientID = -1;
     private static HashMap<Integer, BitSet> bitfields = new HashMap<Integer, BitSet>();
     
     private static boolean initialized = false;
 
-    public static void init(int selfClientID) throws IOException {
-        if (!initialized) {
+    public static void init(int selfClientID) 
+    {
+        if(!initialized) 
+        {
             calculatePieceAmt();
             
             List<Integer> allPeerIDs = ConfigReader.getAllPeerIDs();
-            for (Integer peerID : allPeerIDs) {
+            for (Integer peerID : allPeerIDs) 
+            {
                 bitfields.put(peerID, new BitSet());
             }
-            
             initialized = true;
         }
     }
 
-    private static void calculatePieceAmt() {
-        if (pieceAmt == -1) {
-            int fileSize = ConfigReader.getFileSize();
-            int pieceSize = ConfigReader.getPieceSize();
-    
-            pieceAmt = fileSize / pieceSize;;
-    
-            if (fileSize % pieceSize != 0) {
-                pieceAmt += 1;
-            }
+    private static void calculatePieceAmt() 
+    {
+        if (pieceAmt == -1)
+        {
+        	try {
+				    int fileSize = ConfigReader.getFileSize();
+				    int pieceSize = ConfigReader.getPieceSize();
+   
+				    pieceAmt = fileSize / pieceSize;
+   
+				    if (fileSize % pieceSize != 0) 
+				    {
+				        pieceAmt += 1;
+				    }
+			} 
+        	catch (Exception e) 
+        	{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
         }
     }
 
-    public static void peerReceivedPiece(int peerID, int pieceIndex) {
+    public static void peerReceivedPiece(int peerID, int pieceIndex) 
+    {
         bitfields.get(peerID).set(pieceIndex, true);
     }
 
-    public static void selfReceivedPiece(int pieceIndex) {
+    public static void selfReceivedPiece(int pieceIndex) 
+    {
         bitfields.get(selfClientID).set(pieceIndex, true);
     }
 
-    public static void setPeerBitfield(int peerID, BitSet bitfield) {
+    public static void setPeerBitfield(int peerID, BitSet bitfield) 
+    {
         bitfields.put(peerID, bitfield);
     }
 
-    public static BitSet getPeerBitfield(int peerID) {
+    public static BitSet getPeerBitfield(int peerID) 
+    {
         return bitfields.get(peerID);
     }
 
-    public static BitSet getSelfBitfield() {
+    public static BitSet getSelfBitfield() 
+    {
         return bitfields.get(selfClientID);
     }
 
-    public static byte[] bitfieldToByteArray(BitSet bitfield) {
+    public static byte[] bitfieldToByteArray(BitSet bitfield) 
+    {
         return bitfield.toByteArray();
     }
 
-    public static BitSet byteArrayToBitfield(byte[] bytes) {
+    public static BitSet byteArrayToBitfield(byte[] bytes) 
+    {
         return BitSet.valueOf(bytes);
     }
     
-    public static boolean doesPeerHavePiece(int peerID, int pieceIndex) {
+    public static boolean doesPeerHavePiece(int peerID, int pieceIndex) 
+    {
         return bitfields.get(peerID).get(pieceIndex);
     }
 
-    public static byte[] getSelfBitfieldAsByteArray() {
+    public static byte[] getSelfBitfieldAsByteArray() 
+    {
         return bitfieldToByteArray(getSelfBitfield());
     }
 
-    public static byte[] getBitfieldMessagePayload() {
+    public static byte[] getBitfieldMessagePayload() 
+    {
         return getSelfBitfieldAsByteArray();
+    }
+
+    // does this client need at least 1 piece from the peer with peerID?
+    public static boolean clientNeedsPiecesFromPeer(int otherPeerID) 
+    { 
+        throw new UnsupportedOperationException();
+    }
+
+    public static int getFirstPieceIndexNeedFromPeer(int otherPeerID) 
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    public static void receivedBitfieldMessage(int otherPeerID, byte[] msgPayload) 
+    {
+        BitSet peerBitfield = Bitfield.byteArrayToBitfield(msgPayload);
+		Bitfield.setPeerBitfield(otherPeerID, peerBitfield);
+		if (Bitfield.clientNeedsPiecesFromPeer(otherPeerID)) 
+		{
+			// sendMessage(InterestHandler.GetInterestMessage());
+            PeerProcess.sendMessageToPeer(otherPeerID, InterestHandler.GetInterestMessage());
+		}
+    }
+
+    public static byte[] constructBitfieldMessage(byte[] bitfield) 
+    {
+        // this one is just adding the header (length + message type) to the payload (payload is just the bitfield)
+        throw new UnsupportedOperationException();
     }
 }
