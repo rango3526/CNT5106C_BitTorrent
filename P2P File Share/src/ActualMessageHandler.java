3d288175fd141daacc235e180837faff8ec664c6
Tre' Jeter <33637310+tjeter@users.noreply.github.com>
2021-11-18 21:29:52 -0500

    Update ActualMessageHandler.java

--- P2P File Share/src/ActualMessageHandler.java
+++ P2P File Share/src/ActualMessageHandler.java
@@ -1,19 +1,76 @@
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.nio.ByteBuffer;
 
-public class ActualMessageHandler {
+public class ActualMessageHandler 
+{
     // I think Actual Messages are anything except Handshake messages
-
-    // TODO: Tre'
-
-    public static byte[] constructBitfieldMessage(byte[] bitfield) {
-        // this one is just adding the header (length + message type) to the payload (payload is just the bitfield)
-        throw new UnsupportedOperationException();
+    public static byte[] extractPayload(byte[] fullMessage) 
+    {
+        ByteBuffer bytearray = ByteBuffer.wrap(fullMessage);
+		byte[] msglengthbytes = new byte[4];
+    	byte[] msgtypebytes = new byte[1];
+        bytearray.get(msglengthbytes, 0, msglengthbytes.length);
+        String msgLengthString = new String(msglengthbytes);
+		String msgtypeString = new String(msgtypebytes);
+        if (msgLengthString == "1") 
+        {
+            byte [] msgpayloadbytes = new byte[0];
+            return msgpayloadbytes;
+        } 
+        else 
+        {
+            int msgLengthInt = Integer.parseInt(msgLengthString);
+            byte[] msgpayloadbytes = new byte[msgLengthInt];
+            bytearray.get(msgpayloadbytes, 0, msgpayloadbytes.length);
+            String msgpayloadString = new String(msgpayloadbytes);
+            return msgpayloadbytes;
+        }
     }
-
-    public static byte[] constructChokeMessage(int peerID, boolean choke) { // if choke is false, then unchoke
-        throw new UnsupportedOperationException();
+  
+    //4digit byte array
+    public static byte[] convertIntToBytes(int pieceIndex) 
+	{
+	    return new byte[] 
+	    {
+	        (byte)((pieceIndex >> 24) & 0xff),
+	        (byte)((pieceIndex >> 16) & 0xff),
+	        (byte)((pieceIndex >> 8) & 0xff),
+	        (byte)((pieceIndex >> 0) & 0xff),
+	    };
+	}
+    
+    //1 digit byte array
+    public static byte[] bigIntToByteArray(int pieceIndex) 
+    {
+        BigInteger bigPieceIndex = BigInteger.valueOf(pieceIndex);      
+        return bigPieceIndex.toByteArray();
     }
-
-    public static byte[] extractPayload(byte[] fullMessage) {
+    
+    public static byte[] addHeader(byte[] messagePayload, int messageType) 
+    {
+    	int lengthOfPayload = messagePayload.length + 1;
+    	byte [] newLength = convertIntToBytes(lengthOfPayload);
+    	byte [] newMessageType = bigIntToByteArray(messageType);
+    	ByteArrayOutputStream stream = new ByteArrayOutputStream();
+    	try 
+    	{
+			stream.write(newLength);
+			stream.write(newMessageType);
+	    	stream.write(messagePayload);
+		} 
+    	catch(IOException e) 
+    	{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	byte [] combined = stream.toByteArray();
+    	return (combined);
+    }
+    
+    public static int getMsgType(byte[] msg) 
+    {
         throw new UnsupportedOperationException();
     }
 }
